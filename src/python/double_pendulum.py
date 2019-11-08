-Added double precision include files. -Fixed a numerical error in the CUDA code to simulate the double pendulum.

src/python/double_pendulum.py
@@ -1,7 +1,10 @@
 import pyglet
-from decimal import Decimal
 from pyglet.gl import *
-from math import *
+from pyglet.window import key
+from decimal import Decimal
+import sys
+import numpy as np
+from win32api import GetSystemMetrics
 
 import primitives
 import pyglet_utils
@@ -14,36 +17,37 @@ class PrimaryWindow(pyglet.window.Window):
     timestep = .001
     numStepsToComputePerFrame = int(ceil((1/FPS)/timestep))
     smoothConfig = pyglet_utils.get_smooth_config()
+    labelNumDecimalPlaces = Decimal(10) ** -12
 
-    screenWidth = pyglet.window.get_platform().get_default_display().get_default_screen().width
-    screenHeight = pyglet.window.get_platform().get_default_display().get_default_screen().height
+    screenWidth = GetSystemMetrics(0)
+    screenHeight = GetSystemMetrics(1)
+    windowWidthPixels = int(screenWidth/2)
+    windowHeightPixels = int(screenHeight/2)
 
     pixelsPerMeter = 100
     ballRadiusPixels = 20
     ballColor = (0, 128/255, 255/255, 1)
     gravity = 9.81
-    point1Mass = 10
-    point2Mass = 10
+    point1Mass = 1
+    point2Mass = 1
     pendulum1Length = 1
     pendulum2Length = 1
     point1AngularVelocity = 0
     point2AngularVelocity = 0
 
-    point1Angle = pi+.1
-    point2Angle = 1
+    point1Angle = -pi+.1
+    point2Angle = 0
 
-    origin = [0, 2]
-    originPixels = [origin[0]*pixelsPerMeter + screenWidth/2, origin[1]*pixelsPerMeter + screenHeight/2]
+    origin = [0, 0]
+    originPixels = [origin[0]*pixelsPerMeter + windowWidthPixels/2, origin[1]*pixelsPerMeter + windowHeightPixels/2]
 
     simulationTime = 0
 
     def __init__(self):
-        super(PrimaryWindow, self).__init__(config=self.smoothConfig, style=pyglet.window.Window.WINDOW_STYLE_BORDERLESS)
+        super(PrimaryWindow, self).__init__(config=self.smoothConfig)
         self.set_caption('Double Pendulum')
-        self.set_size(self.screenWidth, self.screenHeight)
-
-        glEnable(GL_BLEND)
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        self.set_size(self.windowWidthPixels, self.windowHeightPixels)
+        self.set_location(int(self.screenWidth/2 - self.windowWidthPixels/2), int(self.screenHeight/2 - self.windowHeightPixels/2))
 
         self.point1 = primitives.Circle(width=self.ballRadiusPixels*2, color=self.ballColor)
         self.point2 = primitives.Circle(width=self.ballRadiusPixels*2, color=self.ballColor)
@@ -52,7 +56,7 @@ class PrimaryWindow(pyglet.window.Window):
 
         self.energyLabel = pyglet.text.Label(font_name='Times New Roman',
                                              font_size=36,
-                                             x=self.screenWidth/2, y=self.screenHeight/2 + 400,
+                                             x=self.windowWidthPixels/2, y=self.windowHeightPixels/2 + 200,
                                              anchor_x='center', anchor_y='center')
 
         pyglet.clock.schedule_interval(self.update, 1.0/self.FPS)
@@ -67,57 +71,43 @@ class PrimaryWindow(pyglet.window.Window):
         self.point2.render()
         self.energyLabel.draw()
 
+    def on_key_press(self, symbol, modifiers):
+        if symbol == key.ESCAPE:
+            sys.exit(0)
 
     def update(self, dt):
 
+        # Compute the next steps of the simulation.
+        for i in range(self.numStepsToComputePerFrame):
+            self.point1AngularVelocity, \
+            self.point2AngularVelocity, \
+            self.point1Angle, \
+            self.point2Angle = compute_double_pendulum_step_rk4(self.point1Mass, self.point2Mass,
+                                                                self.gravity,
+                                                                self.pendulum1Length, self.pendulum2Length,
+                                                                self.point1Angle, self.point2Angle,
+                                                                self.point1AngularVelocity, self.point2AngularVelocity,
+                                                                self.timestep)
+
+
+        self.simulationTime += self.timestep*self.numStepsToComputePerFrame
+
+        # Compute and display the energy of the system.
+        totalEnergy = get_total_energy_of_pendulum(self.origin,
+                                                   self.point1Angle, self.point2Angle,
+                                                   self.point1AngularVelocity, self.point2AngularVelocity,
+                                                   self.pendulum1Length, self.pendulum2Length,
+                                                   self.point1Mass, self.point2Mass,
+                                                   self.gravity)
+        self.energyLabel.text = str(Decimal(totalEnergy).quantize(self.labelNumDecimalPlaces))
+
+        
+        # Update the position of the masses.
         point1Position = get_point_position(self.origin, self.point1Angle, self.pendulum1Length)
         point2Position = get_point_position(point1Position, self.point2Angle, self.pendulum2Length)
 
-        # Compute the potential energy of the masses.
-        potentialEnergy1 = point1Position[1]*self.point1Mass*self.gravity
-        potentialEnergy2 = point2Position[1]*self.point2Mass*self.gravity
-        # potentialEnergy1 = (point1Position[1] - (self.origin[1] - self.pendulum1Length))*self.point1Mass*self.gravity
-        # potentialEnergy2 = (point2Position[1] - (self.origin[1] - self.pendulum1Length - self.pendulum2Length))*self.point2Mass*self.gravity
-
-        # Compute the kinetic energy of the first mass.
-        point1Velocity = self.pendulum1Length*self.point1AngularVelocity
-        kineticEnergyPoint1 = .5*self.point1Mass*point1Velocity**2
-
-        # Compute the kinetic energy of the second mass.
-        point1VelocityX = cos(self.point1Angle)*point1Velocity
-        point1VelocityY = sin(self.point1Angle)*point1Velocity
-        point2LocalVelocity = self.pendulum2Length*self.point2AngularVelocity
-        point2VelocityX = cos(self.point2Angle)*point2LocalVelocity + point1VelocityX
-        point2VelocityY = sin(self.point2Angle)*point2LocalVelocity + point1VelocityY
-        kineticEnergyPoint2 = .5*self.point2Mass*(point2VelocityX**2 + point2VelocityY**2)
-        totalEnergy = potentialEnergy1 + potentialEnergy2 + kineticEnergyPoint1 + kineticEnergyPoint2
-
-
-        # Compute the next steps of the simulation.
-        for i in range(self.numStepsToComputePerFrame):
-            self.point1AngularVelocity, self.point2AngularVelocity, self.point1Angle, self.point2Angle = compute_double_pendulum_step_rk4(self.point1Mass, self.point2Mass,
-                                                                                                                                          self.gravity,
-                                                                                                                                          self.pendulum1Length, self.pendulum2Length,
-                                                                                                                                          self.point1AngularVelocity, self.point2AngularVelocity,
-                                                                                                                                          self.point1Angle, self.point2Angle,
-                                                                                                                                          self.timestep)
-
-            # self.point1AngularVelocity, self.point2AngularVelocity, self.point1Angle, self.point2Angle = compute_double_pendulum_step_euler(self.point1Mass, self.point2Mass,
-            #                                                                                                                             self.gravity,
-            #                                                                                                                             self.pendulum1Length, self.pendulum2Length,
-            #                                                                                                                             self.point1AngularVelocity, self.point2AngularVelocity,
-            #                                                                                                                             self.point1Angle, self.point2Angle,
-            #                                                                                                                             self.timestep)
-
-        self.simulationTime += self.timestep*self.numStepsToComputePerFrame
-        TWOPLACES = Decimal(10) ** -2
-        self.energyLabel.text = str(Decimal(totalEnergy).quantize(TWOPLACES))
-        # self.energyLabel.text = str(Decimal(self.simulationTime).quantize(TWOPLACES))
-
-
-        # Update the position of the masses.
-        point1PositionPixels = (point1Position[0]*self.pixelsPerMeter + self.screenWidth/2, point1Position[1]*self.pixelsPerMeter + self.screenHeight/2)
-        point2PositionPixels = (point2Position[0]*self.pixelsPerMeter + self.screenWidth/2, point2Position[1]*self.pixelsPerMeter + self.screenHeight/2)
+        point1PositionPixels = (point1Position[0]*self.pixelsPerMeter + self.windowWidthPixels/2, point1Position[1]*self.pixelsPerMeter + self.windowHeightPixels/2)
+        point2PositionPixels = (point2Position[0]*self.pixelsPerMeter + self.windowWidthPixels/2, point2Position[1]*self.pixelsPerMeter + self.windowHeightPixels/2)
 
         self.point1.x = point1PositionPixels[0]
         self.point1.y = point1PositionPixels[1]
@@ -128,12 +118,85 @@ class PrimaryWindow(pyglet.window.Window):
         self.line2 = primitives.Line(point1PositionPixels, point2PositionPixels, stroke=5, color=(255,255,255,1))
 
 
+
+
+
+
 def get_point_position(origin, angle, pendulumLength):
     x = sin(angle)*pendulumLength + origin[0]
     y = -cos(angle)*pendulumLength + origin[1]
+
     return [x, y]
 
 
+def get_point_position_np(origin, angle, pendulumLength):
+    x = np.sin(angle)*pendulumLength + origin[0]
+    y = -np.cos(angle)*pendulumLength + origin[1]
+
+    return [x, y]
+
+
+def get_total_energy_of_pendulum(origin, 
+                                 point1Angle, point2Angle,
+                                 point1AngularVelocity, point2AngularVelocity,
+                                 pendulum1Length, pendulum2Length,
+                                 point1Mass, point2Mass,
+                                 gravity):
+    
+    point1Position = get_point_position(origin, point1Angle, pendulum1Length)
+    point2Position = get_point_position(point1Position, point2Angle, pendulum2Length)
+
+    # Compute the potential energy of the masses.
+    potentialEnergy1 = (point1Position[1] + pendulum1Length) * point1Mass * gravity
+    potentialEnergy2 = (point2Position[1] + pendulum1Length + pendulum2Length) * point2Mass * gravity
+    
+    # Compute the kinetic energy of the first mass.
+    point1Velocity = pendulum1Length * point1AngularVelocity
+    kineticEnergyPoint1 = .5 * point1Mass * point1Velocity ** 2
+
+    
+    # Compute the kinetic energy of the second mass.
+    point1VelocityX = cos(point1Angle) * point1Velocity
+    point1VelocityY = sin(point1Angle) * point1Velocity
+    point2LocalVelocity = pendulum2Length * point2AngularVelocity
+    point2VelocityX = cos(point2Angle) * point2LocalVelocity + point1VelocityX
+    point2VelocityY = sin(point2Angle) * point2LocalVelocity + point1VelocityY
+    kineticEnergyPoint2 = .5 * point2Mass * (point2VelocityX ** 2 + point2VelocityY ** 2)
+    totalEnergy = potentialEnergy1 + potentialEnergy2 + kineticEnergyPoint1 + kineticEnergyPoint2
+
+    return totalEnergy
+
+
+def get_total_energy_of_pendulum_np(origin,
+                                    point1Angle, point2Angle,
+                                    point1AngularVelocity, point2AngularVelocity,
+                                    pendulum1Length, pendulum2Length,
+                                    point1Mass, point2Mass,
+                                    gravity):
+
+    point1Position = get_point_position_np(origin, point1Angle, pendulum1Length)
+    point2Position = get_point_position_np(point1Position, point2Angle, pendulum2Length)
+
+    # Compute the potential energy of the masses.
+    potentialEnergy1 = point1Position[1]*point1Mass*gravity
+    potentialEnergy2 = point2Position[1]*point2Mass*gravity
+
+    # Compute the kinetic energy of the first mass.
+    point1Velocity = pendulum1Length * point1AngularVelocity
+    kineticEnergyPoint1 = .5 * point1Mass * point1Velocity ** 2
+
+
+    # Compute the kinetic energy of the second mass.
+    point1VelocityX = np.cos(point1Angle) * point1Velocity
+    point1VelocityY = np.sin(point1Angle) * point1Velocity
+    point2LocalVelocity = pendulum2Length * point2AngularVelocity
+    point2VelocityX = np.cos(point2Angle) * point2LocalVelocity + point1VelocityX
+    point2VelocityY = np.sin(point2Angle) * point2LocalVelocity + point1VelocityY
+    kineticEnergyPoint2 = .5 * point2Mass * (point2VelocityX ** 2 + point2VelocityY ** 2)
+
+    totalEnergy = np.float32(potentialEnergy1 + potentialEnergy2 + kineticEnergyPoint1 + kineticEnergyPoint2)
+
+    return totalEnergy
 
 
 

src/python/double_pendulum_fractal.py
@@ -1,9 +1,13 @@
+import random
 from math import *
 import numpy as np
 from PIL import Image, ImageTk
 import tkinter as tk
 import os
 import time
+
+from win32api import GetSystemMetrics
+
 from utils import read_file
 
 import pycuda.driver as drv
@@ -16,31 +20,43 @@ from pycuda.compiler import SourceModule
 
 class DoublePendulumFractalApp(tk.Tk):
 
-    angle1Min = -pi
-    angle1Max = pi
-    angle2Min = -pi
-    angle2Max = pi
-    numberOfAnglesToTestPerKernelCallRatio = 4
-    numberOfAnglesToTestX = 1000
-    numberOfAnglesToTestY = 1000
-    timestep = .001
-    maxTimeToSeeIfPendulumFlips = 8
-    zoomFactor = 2
+    # The range of angles.
+    angle1Min = 1/2*pi
+    angle1Max = 3/2*pi
+    angle2Min = 0
+    angle2Max = 2*pi
 
+    # Number of pixels for the X and Y axes.
+    numberOfAnglesToTestX = int(500 / 2**0)
+    numberOfAnglesToTestY = int(1000 / 2**0)
+
+    # Simulation parameters.
+    timestep = .001
+    maxTimeToSeeIfPendulumFlips = 2**5
+    zoomFactor = 2
+    numberOfAnglesToTestPerKernelCallRatio = 1
+
+    # Pendulum model parameters.
     gravity = 9.81
-    point1Mass = 10
-    point2Mass = 10
+    point1Mass = 1
+    point2Mass = 1
     pendulum1Length = 1
     pendulum2Length = 1
 
-    colors = np.zeros((3, numberOfAnglesToTestY, numberOfAnglesToTestX), np.dtype(np.uint8))
-
     # Initialize the kernel.
+    useDoublePrecision = False  # Enabling double precision could slow performance by 20 times or more.
     includeDir = os.getcwd() + '/include'
-    doublePendulumFunction = SourceModule(read_file('src/cuda/double_pendulum_fractal.cu'), include_dirs=[includeDir]).get_function('compute_double_pendulum_fractal_image')
+    kernelFile = 'src/cuda/double_pendulum_fractal.cu' if not useDoublePrecision else 'src/cuda/double_pendulum_fractal_double_precision.cu' 
+    doublePendulumFractalFunction = SourceModule(read_file(kernelFile), include_dirs=[includeDir]).get_function('compute_double_pendulum_fractal_image')
 
     def __init__(self):
         tk.Tk.__init__(self)
+
+        # Set the window size and position.
+        self.geometry('%dx%d+%d+%d' % (self.numberOfAnglesToTestX,
+                                       self.numberOfAnglesToTestY,
+                                       GetSystemMetrics(0)/2 - self.numberOfAnglesToTestX/2,
+                                       GetSystemMetrics(1)/2 - self.numberOfAnglesToTestY/2))
 
         self.canvas = tk.Canvas(self, width=self.numberOfAnglesToTestX, height=self.numberOfAnglesToTestY)
         self.canvas.pack(side='top', fill='both', expand=True)
@@ -62,10 +78,10 @@ class DoublePendulumFractalApp(tk.Tk):
         self.angle2Min = center2 - newHeight / 2
         self.angle2Max = center2 + newHeight / 2
 
-        print('self.angle1Min = ' + str(self.angle1Min))
-        print('self.angle1Max = ' + str(self.angle1Max))
-        print('self.angle2Min = ' + str(self.angle2Min))
-        print('self.angle2Max = ' + str(self.angle2Max))
+        print('angle1Min = ' + str(self.angle1Min))
+        print('angle1Max = ' + str(self.angle1Max))
+        print('angle2Min = ' + str(self.angle2Min))
+        print('angle2Max = ' + str(self.angle2Max))
 
         self.draw_fractal()
         
@@ -87,55 +103,82 @@ class DoublePendulumFractalApp(tk.Tk):
     def draw_fractal(self):
 
         print('Drawing fractal')
-        
-        # Compute the double pendulum fractal image.
-        colorsGpu = drv.mem_alloc(self.colors.nbytes)
 
+        # Compute the double pendulum fractal image.
         start = time.time()
 
+        colors = np.zeros((3, self.numberOfAnglesToTestY, self.numberOfAnglesToTestX), np.dtype(np.uint8))
         for i in range(self.numberOfAnglesToTestPerKernelCallRatio):
             for j in range(self.numberOfAnglesToTestPerKernelCallRatio):
 
-                self.doublePendulumFunction(np.float32(self.point1Mass), np.float32(self.point2Mass),
-                                            np.float32(self.pendulum1Length), np.float32(self.pendulum2Length),
-                                            np.float32(self.gravity),
-                                            np.float32(self.angle1Min), np.float32(self.angle1Max),
-                                            np.float32(self.angle2Min), np.float32(self.angle2Max),
-                                            np.int32(self.numberOfAnglesToTestPerKernelCallRatio),
-                                            np.int32(i), np.int32(j),
-                                            np.int32(self.numberOfAnglesToTestX), np.int32(self.numberOfAnglesToTestY),
-                                            np.float32(self.timestep),
-                                            np.float32(self.maxTimeToSeeIfPendulumFlips),
-                                            colorsGpu,
-                                            # block=(4, 4, 1), grid=(4, 4))
-                                            # block=(8, 8, 1), grid=(8, 8))
-                                            block=(16, 16, 1), grid=(16, 16))
-                                            # block=(32, 32, 1), grid=(32, 32))
+                print('Running kernel...')
+
+                kernelStart = time.time()
+                curColors = np.zeros_like(colors)
+                
+                if not self.useDoublePrecision:
+                    self.doublePendulumFractalFunction(np.float32(self.point1Mass), np.float32(self.point2Mass),
+                                                       np.float32(self.pendulum1Length), np.float32(self.pendulum2Length),
+                                                       np.float32(self.gravity),
+                                                       np.float32(self.angle1Min), np.float32(self.angle1Max),
+                                                       np.float32(self.angle2Min), np.float32(self.angle2Max),
+                                                       np.int32(self.numberOfAnglesToTestPerKernelCallRatio),
+                                                       np.int32(i), np.int32(j),
+                                                       np.int32(self.numberOfAnglesToTestX), np.int32(self.numberOfAnglesToTestY),
+                                                       np.float32(self.timestep),
+                                                       np.float32(self.maxTimeToSeeIfPendulumFlips),
+                                                       drv.Out(curColors),
+                                                       # colorsGpu,
+                                                       # block=(4, 4, 1), grid=(4, 4))
+                                                       # block=(8, 8, 1), grid=(8, 8))
+                                                       block=(16, 16, 1), grid=(16, 16))
+                                                # block=(32, 32, 1), grid=(32, 32))
+                                                # block=(2, 2, 1), grid=(1, 1))
+                
+                else:
+                    self.doublePendulumFractalFunction(np.float64(self.point1Mass), np.float64(self.point2Mass),
+                                                       np.float64(self.pendulum1Length), np.float64(self.pendulum2Length),
+                                                       np.float64(self.gravity),
+                                                       np.float64(self.angle1Min), np.float64(self.angle1Max),
+                                                       np.float64(self.angle2Min), np.float64(self.angle2Max),
+                                                       np.int32(self.numberOfAnglesToTestPerKernelCallRatio),
+                                                       np.int32(i), np.int32(j),
+                                                       np.int32(self.numberOfAnglesToTestX), np.int32(self.numberOfAnglesToTestY),
+                                                       np.float64(self.timestep),
+                                                       np.float64(self.maxTimeToSeeIfPendulumFlips),
+                                                       drv.Out(curColors),
+                                                       # colorsGpu,
+                                                       # block=(4, 4, 1), grid=(4, 4))
+                                                       # block=(8, 8, 1), grid=(8, 8))
+                                                       block=(16, 16, 1), grid=(16, 16))
+                                                # block=(32, 32, 1), grid=(32, 32))
+                                                # block=(2, 2, 1), grid=(1, 1))
 
 
-                drv.Context.synchronize()
-                print('Completed ' + str(int(i*self.numberOfAnglesToTestPerKernelCallRatio + j + 1)) + ' out of ' +  str(int(pow(self.numberOfAnglesToTestPerKernelCallRatio, 2))) + ' kernels')
+                colors = np.add(colors, curColors)
+                
+                print('Completed ' + str(int(i*self.numberOfAnglesToTestPerKernelCallRatio + j + 1)) + ' out of ' +
+                      str(int(pow(self.numberOfAnglesToTestPerKernelCallRatio, 2))) + ' kernels in ' + str(time.time() - kernelStart) + ' seconds')
 
 
-        print('total kernel time = ' + str(time.time() - start))
 
-        start = time.time()
-        drv.memcpy_dtoh(self.colors, colorsGpu)
-
-        print('copy time = ' + str(time.time() - start))
+        print('total time to run kernel = ' + str(time.time() - start))
 
         # Display the image.
-        redArray = Image.fromarray(self.colors[0])
-        greenArray = Image.fromarray(self.colors[1])
-        blueArray = Image.fromarray(self.colors[2])
+        redArray = Image.fromarray(colors[0])
+        greenArray = Image.fromarray(colors[1])
+        blueArray = Image.fromarray(colors[2])
 
         pilImageRGB = Image.merge('RGB', (redArray, greenArray, blueArray))
         img = ImageTk.PhotoImage(pilImageRGB)
-
         self.canvas.create_image((self.numberOfAnglesToTestX / 2, self.numberOfAnglesToTestY / 2), image=img, state="normal")
 
         # Save the image so it isn't garbage collected.
         self.image = img
+
+        # Save the image to a file.
+        filename = "double pendulum fractal.png"
+        pilImageRGB.save(filename)
 
 
 